ca95ab685866f96e1861c64679e6e66e8db31448
rufevean <deerajmydearalways@gmail.com>
2024-06-25 18:11:57 +0530

    minor changes

--- src/layouts/splashScreen.jsx
+++ src/layouts/splashScreen.jsx
@@ -13,11 +13,11 @@ function SplashScreen() {
       <content className="splash-content">
         <img src={logo} alt="logo" className="logo"/>
         <h1 className="splash-title">What is <span className="splash-highlight">PAL</span> ?</h1>
+      
+        <p className="splash-text"><span className="splash-highlight" >P</span>an <span className="splash-highlight" >A</span>pplication <span className="splash-highlight" >L</span>inguistic  —  <span className="splash-italic">ideal for any field </span> </p> 
+      <p className="splash-text"> <span className="splash-highlight" >P</span>seudocode <span className="splash-highlight" >A</span>bstraction <span className="splash-highlight" >L</span>ayer  —  <span className="splash-italic"> readable to anyone </span></p>
+
         <p className="splash-text"> <span className="splash-highlight" >P</span>erformant <span className="splash-highlight" >A</span>ugmentable <span className="splash-highlight" >L</span>anguage  — <span className="splash-italic">fast and extensible </span> </p>
-        <p className="splash-text"> <span className="splash-highlight" >P</span>seudocode <span className="splash-highlight" >A</span>bstraction <span className="splash-highlight" >L</span>ayer  —  <span className="splash-italic"> readable to anyone </span></p>
-        <p className="splash-text"><span className="splash-highlight" >P</span>an <span className="splash-highlight" >A</span>pplication <span className="splash-highlight" >L</span>inguistic  —  <span className="splash-italic">ideal for any field </span> </p>
-        <p className="splash-text"> <span className="splash-highlight" >P</span>roblem <span className="splash-highlight" >A</span>daptive <span className="splash-highlight" >L</span>ocus  —  <span className="splash-italic"> custom fits the problem domain  </span> </p>
-        <p className="splash-text"><span className="splash-highlight" >P</span>ortable <span className="splash-highlight" >A</span>rchitecture <span className="splash-highlight" >L</span>ayout  —  <span className="splash-italic" >hardware/OS agnostic </span> </p>
         <p className="splash-text"> <span className="splash-highlight" >PAL</span> —  <span className="splash-italic" > a true friend indeed to the computer programmer </span> </p>
         <div className="splash-enter"><span className="splash-highlight" >[ </span> Enter to continue <span className="splash-highlight" >] </span> </div>
       </content> 
