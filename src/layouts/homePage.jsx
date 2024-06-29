ea30199e187b93bc35351f889ae811b5a303cad1
rufevean <deerajmydearalways@gmail.com>
2024-06-29 21:20:04 +0530

    minor changes

--- src/layouts/homePage.jsx
+++ src/layouts/homePage.jsx
@@ -2,13 +2,19 @@ import '../styles/home-page.css'
 import React from 'react';
 import NavBar from './navBar';
 import '../styles/animated-static-bg.css';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
   return (
     <div className="home-page">
 
       <NavBar /> 
-      WIP 
+      <div className="home-content">
+      <div> PAL-Tek Aims to develop a Programming Language that is like no other. </div>
+      <div> Check out these articles to learn more about the PAL-Tek Programming Language. </div>
+    <Link to="/features"> Articles </Link>
+<div > WORK IN PROGRESS</div>
+      </div>
     </div>
   );
 }
