5c0a9264e99e83fe545cb8d950b4727048b1df2e
rufevean <deerajmydearalways@gmail.com>
2024-08-13 09:03:40 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './', // Root directory of your project
-  publicDir: 'public', // Public directory where the Docusaurus build is copied
+  root: './',
+  publicDir: 'public',
   server: {
-    port: 5174, // Preferred port for your Vite server
+    port: 5174,
+    proxy: {
+      '/testflare': {
+        target: 'https://paltek.assistflare.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/testflare/, ''),
+      },
+    },
   },
 });
-;
